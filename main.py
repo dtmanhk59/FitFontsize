514637f29bcbcfb96ddd2316d5e3f8326dbf9650
Duong Manh <duongtienmanh8121995@gmail.com>
2020-11-21 04:37:48 +0000

    update

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pdfplumber
 
+class TextBox:
+  def __init__(self, char):
+    if len(char['text'].strip()) != 0:
+      self.text = char['text']
+      self.x = float(char['x0'])
+      self.y = float(char['y0'])
+      self.width = float(char['width'])
+      self.height = float(char['height'])
+      self.font_name = char['fontname']
+      self.font_size = float(char['size'])
+      self.page_number = char['page_number']
+
+
 class Pdf2Box:
   def __init__(self, file_name):
     with pdfplumber.open(file_name) as reader:
       for page in reader.pages:
-        print(page)
+        for char in page.chars:
+          text_box = TextBox(char)
+          print(text_box.__dict__)
 
 
 def test_Pdf2Box__init__():
